6ee643da8ff6cb5e41ef4888df350def923acc75
kshitijofficial <srivastavakshitijprofessional@gmail.com>
2024-09-28 12:38:12 +0530

    candidate registration fix

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,15 +1,19 @@
 import { Link } from "react-router-dom";
+import "./Navigation.css";
 const Navigation = () =>{
     return(
-        <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/register-voter">Register Voter</Link></li>
-        <li><Link to="/register-candidate">Register Candidate</Link></li>
-        <li><Link to="/voter-list">Voter List</Link></li>
-        <li><Link to="/candidate-list">Candidate List</Link></li>
-        <li><Link to="/election-commision">Election Commision</Link></li>
-    </ul>
-  
+    <header>
+      <nav className="navbar">
+        <ul className="navbar__list">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/register-voter">Register Voter</Link></li>
+            <li><Link to="/register-candidate">Register Candidate</Link></li>
+            <li><Link to="/voter-list">Voter List</Link></li>
+            <li><Link to="/candidate-list">Candidate List</Link></li>
+            <li><Link to="/election-commision">Election Commision</Link></li>
+        </ul>
+      </nav>
+    </header>
     )
  
 }
